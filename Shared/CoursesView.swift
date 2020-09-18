dda0c4fa8d12ba228703c62a604f4f5b9854ab96
Tprezioso <tommyprezioso@gmail.com>
2020-09-18 17:46:18 -0400

    first commit

---
+++ Shared/CoursesView.swift
@@ -0,0 +1,21 @@
+//
+//  CoursesView.swift
+//  DesignCodeCourse
+//
+//  Created by Thomas Prezioso on 9/11/20.
+//
+
+import SwiftUI
+
+struct CoursesView: View {
+    var body: some View {
+        Text("")
+    
+    }
+}
+
+struct CoursesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoursesView()
+    }
+}
