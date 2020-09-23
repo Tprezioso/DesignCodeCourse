655409d41cdb63fd2aa5884e5e8b06ad6667c51f
Tprezioso <tommyprezioso@gmail.com>
2020-09-23 19:20:48 -0400

    Showing a view and transitions

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct CoursesView: View {
+    @State var show = false
     var body: some View {
-        Text("")
-    
+        ZStack {
+            CourseItem()
+                .frame(width: 335, height:250)
+            
+            VStack {
+                if show {
+                    CourseItem()
+                        .transition(.move(edge: .leading))
+                        .ignoresSafeArea([.all])
+                }
+                
+            }
+        }
+        .onTapGesture {
+            withAnimation(.spring()) {
+                show.toggle()
+
+            }
+        }
+//        .animation(.spring())
+        
     }
 }
 
